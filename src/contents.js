6cf6ed461d5407040106eab7a72cee1dbed95459
S4WA <s4wa@outlook.com>
2019-05-13 21:33:34 +0900

    Update files
    もうよくわかんないめう～

--- src/contents.js
+++ src/contents.js
@@ -1,8 +1,7 @@
 var json = {
 	"playing": false,
 	"track": null,
-	"artwork": null,
-	"queue": null
+	"artwork": null
 };
 
 window.onload = () => {
@@ -10,19 +9,35 @@ window.onload = () => {
 	setInterval(()=> {
 		ready = (document.getElementsByClassName("playControls__wrapper l-container l-fullwidth").length != 0);
 		if (ready) {
-			chrome.storage.sync.set(json, () => {});
+			// send
+			json["playing"] = document.getElementsByClassName("playControl sc-ir playControls__control playControls__play").length != 0;
+			json["track"] = document.title.replace("▶ ", "").replace(" | Free Listening on SoundCloud", "");
+			json["artwork"] = document.getElementsByClassName("playbackSoundBadge")[0].children[0].children[0].children[0].style.backgroundImage.replace("url(\"", "").replace("\")", "").replace("50x50.", "500x500.");
+
+			chrome.storage.sync.set(json, null);
 		}
-	}, 100);
+	}, 500);
 }
 
-/*
-	format: json
-	value: default
-
-	{
-		"playing": false,
-		"track": null, // or "track-name by someone"
-		"artwork": null, // or "img url"
-		"queue": null // or "next / prev"
+chrome.runtime.onMessage.addListener((request, sender, callback) => {
+	console.log(request);
+	switch(request.toLowerCase()) {
+		case "play":
+		case "pause": {
+			document.getElementsByClassName("playControl sc-ir playControls__control playControls__play")[0].click();
+			break;
+		}
+		case "prev": {
+			document.getElementsByClassName("skipControl sc-ir playControls__control playControls__prev skipControl__previous")[0].click();
+			break;
+		}
+		case "next": {
+			document.getElementsByClassName("skipControl sc-ir playControls__control playControls__next skipControl__next")[0].click();
+			break;
+		}
+		default: {
+			break;
+		}
 	}
-*/
+	sendResponse(true);
+});
--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,2 +1,33 @@
-chrome.storage.sync.get(null, (items) => {
-});
+var artworkElem, trackElem, toggleElem, prevElem, nextElem;
+window.onload = () => {
+	artworkElem = document.getElementById("artwork");
+	trackElem = document.getElementById("track");
+	toggleElem = document.getElementById("toggle");
+	prevElem = document.getElementById("prev");
+	nextElem = document.getElementById("next");
+
+	toggleElem.addEventListener("click", toggle());
+	prevElem.addEventListener("click", queue("prev"));
+	nextElem.addEventListener("click", queue("next"));
+
+	setInterval(()=> {
+		chrome.storage.sync.get(null, (items) => {
+			if (items["artwork"] != null && items["artwork"] != artworkElem.src) artworkElem.src = items["artwork"];
+			if (items["track"] != null && items["track"] != trackElem.innerText) trackElem.innerText = items["track"];
+			if (items["playing"] != null) toggleElem.value = !items["playing"] ? "Play" : "Pause";
+		});
+	}, 500);
+}
+
+function queue(request) {
+	chrome.tabs.query({active: true, url: "*://soundcloud.com"}, (tab) => {
+		chrome.tabs.sendMessage(tab[0].id, request.toLowerCase(), null);
+	});
+}
+
+function toggle() {
+	if (toggleElem == null) return;
+	var rString = toggleElem.value == "Pause" ? "Play" : "Pause";
+	queue(toggleElem.value);
+	toggleElem.value = rString;
+}
