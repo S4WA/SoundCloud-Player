8d962452431cda9c08893b80ccd0d0302cc70fa2
S4WA <s4wa@outlook.com>
2019-05-27 04:29:57 +0900

    圧縮
    #3

---
+++ exported/popup/popup.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded",()=>{init()});window.onload=()=>{setInterval(updater(),500);ready=!0}
+function updater(){chrome.storage.sync.get(null,function(items){if(!ready)return;if((items.artwork!=null&&items.artwork!="")&&items.artwork!=artworkElem.src){artworkElem.src=items.artwork}
+if(items.track!=null&&items.track!=trackElem.innerText){trackElem.innerText=items.track}
+if(items.playing!=null){toggleElem.value=!items.playing?"Play":"Pause"}
+if(items.favorite!=null){favElem.value=!items.favorite?"Fav":"unFav"}
+if(items.shuffle!=null){shuffleElem.value=items.shuffle?"Shuffled":"Shuffle"}
+if(items.repeat!=null){repeatElem.value=items.repeat=="none"?"Repeat":"Repeat ("+items.repeat+")"}
+if(items.time!=null){var timeJson=items.time;if($("#current").text()!=timeJson.current){$("#current").text(timeJson.current)}
+if($("#end").text()!=timeJson.end){$("#end").text(timeJson.end)}}
+if(items.volume!=null){$("#volume").text(items.volume+" %")}
+json=items})}
+function init(){chrome.storage.sync.get(null,(items)=>{json=items});chrome.tabs.query({url:"*://soundcloud.com/*"},(results)=>{if(results.length==0){ready=!1;json.artwork="";json.track="* Click to Open SoundCloud *";json.time=null;json.volume=0;chrome.storage.sync.set(json,null);$("#close").remove();$("#second br:last-child").remove();$("#controller").remove()}});$("#version").text("v"+chrome.runtime.getManifest().version);registerElements();registerEvents()}
+function registerEvents(){$(toggleElem).on("click",()=>{toggle()});$(prevElem).on("click",()=>{queue("prev")});$(nextElem).on("click",()=>{queue("next")});$(favElem).on("click",()=>{toggleFav()});$(trackElem).on("click",()=>{openSCTab()});$(artworkElem).on("click",()=>{openSCTab()});$(repeatElem).on("click",()=>{repeat()});$(shuffleElem).on("click",()=>{queue("shuffle")});$("#store").on("click",()=>{openURL("https://chrome.google.com/webstore/detail/soundcloud-player/oackhlcggjandamnkggpfhfjbnecefej")});$("#drip").on("click",()=>{openURL("https://twitter.com/AkibaKaede")});$("#close").on("click",()=>{chrome.tabs.query({url:"*://soundcloud.com/*"},(results)=>{if(results.length!=0){chrome.tabs.remove(results[0].id,()=>{})}})})}
+function queue(request){if(!ready)return;request=request.toLowerCase();chrome.tabs.query({url:"*://soundcloud.com/*"},(results)=>{if(results.length!=0)chrome.tabs.sendMessage(results[0].id,request.toLowerCase(),null)})}
+function openSCTab(){chrome.tabs.query({url:"*://soundcloud.com/*"},(results)=>{if(results.length!==0){chrome.tabs.update(results[0].id,{active:!0},(tab)=>{})}else{chrome.tabs.create({url:"https://soundcloud.com"},(tab)=>{})}
+window.close()})}
+function openURL(link){chrome.tabs.create({url:link},(tab)=>{})}
+function toggleFav(){if(favElem==null)return;var string=favElem.value=="Fav"?"unFav":"Fav";queue(favElem.value=string)}
+function repeat(){if(repeatElem==null)return;repeatElem.value=(json.repeat==null&&json.repeat!="none"?"Repeat":"Repeat ("+json.repeat+")");queue("repeat")}
+function toggle(){if(toggleElem==null)return;var string=toggleElem.value=="Pause"?"Play":"Pause";queue(toggleElem.value=string)}
+function registerElements(){artworkElem=$("#artwork")[0];trackElem=$("#track")[0];toggleElem=$("#toggle")[0];prevElem=$("#prev")[0];nextElem=$("#next")[0];favElem=$("#fav")[0];repeatElem=$("#repeat")[0];shuffleElem=$("#shuffle")[0]}
+var ready=!1,json={},artworkElem,trackElem,toggleElem,prevElem,nextElem,favElem,repeatElem,shuffleElem
--- src/contents.js
+++ src/contents.js
@@ -1,14 +1,104 @@
-window.onload=()=>{var ready=!1;setInterval(()=>{ready=($(".playControls__wrapper").length!=0);var playing=$(".playControl")[0].title=="Pause current",track=$("a.playbackSoundBadge__titleLink")[0].title+" By "+$("a.playbackSoundBadge__lightLink")[0].title,artwork=$(".playbackSoundBadge span.sc-artwork").css("background-image").replace("url(\"","").replace("\")","").replace("50x50.","500x500."),fav=$(".playControls__soundBadge .sc-button-like")[0].title=="Unlike",current=$(".playbackTimeline__timePassed span[aria-hidden]").text(),end=$(".playbackTimeline__duration span[aria-hidden]").text(),volume=Number($(".volume__sliderWrapper").attr("aria-valuenow"))*100;if(ready&&json.track!=track){json.track=track;json.artwork=artwork;post()}
-if(json.playing!=playing){json.playing=playing;post()}
-if(json.favorite!=fav){json.favorite=fav;post()}
-if(json.time.current!=current){json.time.current=current;post()}
-if(json.time.end!=end){json.time.end=end;post()}
-if(json.volume!=volume){json.volume=volume;post()}},500)}
-function post(){chrome.storage.sync.set(json,null)}
-chrome.runtime.onMessage.addListener((request,sender,callback)=>{switch(request.toLowerCase()){case "play":case "pause":{$(".playControl.sc-ir.playControls__control.playControls__play")[0].click();json.playing=!json.playing;post();break}
-case "prev":{$(".playControls__prev")[0].click();break}
-case "next":{$(".playControls__next")[0].click();break}
-case "unfav":case "fav":{$(".playbackSoundBadge__like")[0].click();json.favorite=$(".playbackSoundBadge__like")[0].title=="Unlike";post();break}
-case "repeat":{var btn=$(".repeatControl")[0];btn.click();json.repeat=btn.className.replace("repeatControl sc-ir m-","").toLowerCase();post();break}
-case "shuffle":{var btn=$(".shuffleControl")[0];btn.click();json.shuffle=btn.className.includes("m-shuffling");post();break}
-default:{break}}});var json={"playing":!1,"track":null,"artwork":null,"favorite":!1,"shuffle":!1,"repeat":"none","time":{"current":null,"end":null},"volume":0}
+window.onload = () => {
+	var ready = false;
+	setInterval(() => {
+		ready = ($(".playControls__wrapper").length != 0);
+
+		var playing = $(".playControl")[0].title == "Pause current",
+			track = $("a.playbackSoundBadge__titleLink")[0].title + " By " + $("a.playbackSoundBadge__lightLink")[0].title,
+			artwork = $(".playbackSoundBadge span.sc-artwork").css("background-image").replace("url(\"", "").replace("\")", "").replace("50x50.", "500x500."),
+			fav = $(".playControls__soundBadge .sc-button-like")[0].title == "Unlike",
+			current = $(".playbackTimeline__timePassed span[aria-hidden]").text(),
+			end = $(".playbackTimeline__duration span[aria-hidden]").text(),
+			volume = Number($(".volume__sliderWrapper").attr("aria-valuenow"))*100;
+
+		if (ready && json["track"] != track) {
+			json["track"] = track;
+			json["artwork"] = artwork;
+			post();
+		}
+		if (json["playing"] != playing) {
+			json["playing"] = playing;
+			post();
+		}
+		if (json["favorite"] != fav) {
+			json["favorite"] = fav;
+			post();
+		}
+		if (json["time"]["current"] != current) {
+			json["time"]["current"] = current;
+			post();
+		}
+		if (json["time"]["end"] != end) {
+			json["time"]["end"] = end;
+			post();
+		}
+		if (json["volume"] != volume) {
+			json["volume"] = volume;
+			post();
+		}
+
+	}, 500);
+}
+
+function post() {
+	chrome.storage.sync.set(json, null);
+}
+
+chrome.runtime.onMessage.addListener((request, sender, callback) => {
+	// console.log(request);
+	switch(request.toLowerCase()) {
+		case "play":
+		case "pause": {
+			$(".playControl.sc-ir.playControls__control.playControls__play")[0].click();
+			json["playing"] = !json["playing"];
+			post();
+			break;
+		}
+		case "prev": {
+			$(".playControls__prev")[0].click();
+			break;
+		}
+		case "next": {
+			$(".playControls__next")[0].click();
+			break;
+		}
+		case "unfav":
+		case "fav": {
+			$(".playbackSoundBadge__like")[0].click();
+			json["favorite"] = $(".playbackSoundBadge__like")[0].title == "Unlike";
+			post();
+			break;
+		}
+		case "repeat": {
+			var btn = $(".repeatControl")[0];
+			btn.click();
+			json["repeat"] = btn.className.replace("repeatControl sc-ir m-", "").toLowerCase(); // none -> one -> all
+			post();
+			break;
+		}
+		case "shuffle": {
+			var btn = $(".shuffleControl")[0];
+			btn.click();
+			json["shuffle"] = btn.className.includes("m-shuffling");
+			post();
+			break;
+		}
+		default: {
+			break;
+		}
+	}
+});
+
+var json = {
+	"playing": false,
+	"track": null,
+	"artwork": null,
+	"favorite": false,
+	"shuffle": false,
+	"repeat": "none",
+	"time": {
+		"current": null,
+		"end": null
+	},
+	"volume": 0
+};
