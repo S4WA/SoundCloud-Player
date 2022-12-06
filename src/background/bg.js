bd1dee7dc8641797f3f5df0dfe8bf1268b4d2d57
Drip <drip@outlook.jp>
2022-12-07 00:16:11 +0900

    Update bg.js

--- src/background/bg.js
+++ src/background/bg.js
@@ -17,29 +17,29 @@ const names = [
   'seekf' 
 ];
 
-this.browser.commands.onCommand.addListener(async (command) => {
+browser.commands.onCommand.addListener(async (command) => {
   command = command.toLowerCase();
   if (!names.includes(command)) return;
   // console.log(command);
   if (command == 'open') {
-    let [ScTab] = await this.browser.tabs.query({ url: '*://soundcloud.com/*' });
-    let [currentTab] = await this.browser.tabs.query({ active: true, lastFocusedWindow: true });
+    let [ScTab] = await browser.tabs.query({ url: '*://soundcloud.com/*' });
+    let [currentTab] = await browser.tabs.query({ active: true, lastFocusedWindow: true });
 
     if (!currentTab) {
       return;
     }
     
     if (!ScTab) {
-      await this.browser.tabs.create({ url: "https://soundcloud.com/" });
+      await browser.tabs.create({ url: "https://soundcloud.com/" });
       return;
     }
 
     if (currentTab.windowId != ScTab.windowId) {
-      await this.browser.windows.update(ScTab.windowId, { focused: true });
+      await browser.windows.update(ScTab.windowId, { focused: true });
     }
 
     if (currentTab.id != ScTab.id) {
-      await this.browser.tabs.update(ScTab.id, { active: true });
+      await browser.tabs.update(ScTab.id, { active: true });
     } else {
       await queue('open');
     }
@@ -50,9 +50,9 @@ this.browser.commands.onCommand.addListener(async (command) => {
 
 async function queue(command) {
   return new Promise(async(resolve, reject) => {
-    let results = await this.browser.tabs.query({ url: '*://soundcloud.com/*' });
+    let results = await browser.tabs.query({ url: '*://soundcloud.com/*' });
     if (results != null && results.length != 0 && results[0].status == 'complete') {
-      resolve(this.browser.tabs.sendMessage(results[0].id, {'type': command}));
+      resolve(browser.tabs.sendMessage(results[0].id, {'type': command}));
     }
   });
 } 
